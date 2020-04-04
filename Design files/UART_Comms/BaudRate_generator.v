aeafff716651bc2a4b2d8a9a57a615a66c42ddd2
Aravantinos Christos <aravantinos.chris.oficial@gmail.com>
2020-04-04 23:34:11 +0300

    Designed the Transmition module
    Almost ready without a testbench for the prototyping though . It's gonna be ready soon and next is the receiver.

--- Design files/UART_Comms/BaudRate_generator.v
+++ Design files/UART_Comms/BaudRate_generator.v
@@ -1,9 +1,10 @@
 /* In binary rates Baud rate is the same as Bit rate. There are plenty of Baud rates we can choose.
-Not all of them work in the windows comms. Our FPGA for this project is a Nexys-4 Artix-7 with a clock frequency of 100MHz. The baud rate we want to work is 14400. 
-If we want the Ticks to have 16 times the frequency of the UART signal we need a frequenct 16 times the 14400Hz.
+Not all of them work in the windows comms. Our FPGA for this project is a Nexys-4 Artix-7 with a clock frequency of 100MHz. 
+The baud rate we want to work is 14400. 
+If we want the Ticks to be 16 times the frequency of the UART signal we need a frequenct 16 times the 14400Hz.
 
-The UART signal width is 1/14400 with is equal to 69,4 us. The period of the clock is 1/100MhHz equal to 10ns.
-To get 16 ticks we need to count (69400ns/16)/10ns = 434,02 pulses. The error % is small enough to be neglected.
+The UART signal width is 1/14400 which is equal to 69,4 us. The period of the clock is 1/100MhHz equal to 10ns.
+To get a tick we need to count (69400ns/16)/10ns = 434,02 pulses. The error % is small enough to be neglected.
 Our top file's Baud Rate is set to 434.
 */
 
@@ -19,9 +20,9 @@ assign Tick = (Baudrate_count == Baudrate);
 
 always @(posedge  clock or posedge reset) begin
 
-	if(reset) Baudrate_count<= 16'b1;
+	if(reset) Baudrate_count <= 16'b1;
 
-	else if (Tick) Baudrate_count<= 16'b1;
+	else if (Tick) Baudrate_count <= 16'b1;
 
 	else Baudrate_count <= Baudrate_count + 1'b1;
 
