5012721ceb08a2d91c43ea388a3992afff21a502
Aravantinos Christos <aravantinos.chris.oficial@gmail.com>
2020-04-09 04:23:43 +0300

    Transmittion module Update
    We need to run a full benchmark and possibly make a prototype on the Nexys 4 Artix 7 but we are very close to complete the tx module of the UART interface

--- Design files/UART_Comms/UART_Tx.v
+++ Design files/UART_Comms/UART_Tx.v
@@ -1,4 +1,4 @@
-module UART_Tx( input   Clock, Reset, Tx_enable,Tick,
+module UART_Tx( input   Clock, Reset, Tx_en,Tick,
                 input [7:0]               Message_in, 
 		input [3:0]                   N_bits,
 		output               Tx_out, Tx_done  );
@@ -7,17 +7,85 @@ module UART_Tx( input   Clock, Reset, Tx_enable,Tick,
 parameter IDLE = 1'b0 , SEND = 1'b1;    // FSM States
 
 
-reg            c_state , n_state, write_en, done, t_out;
+reg            c_state, n_state, write_en, done, t_out;
 reg [3:0]      counter        = 4'b0;
 reg            start_bit      = 1'b1;
 reg            stop_bit       = 1'b0;
 reg [7:0]      msg_in_reg     = 8'b0;
 reg [3:0]      bit_counter    = 4'b0;
-
+reg [1:0]      d_shift_reg;
+wire           Dbnce;
 
 assign Tx_done = done;
 assign Tx_out = t_out;
- 
+
+
+
+///////State Machine////////
+////////////////////////////
+always @(posedge Clock or posedge Reset) begin 
+
+	if(Reset) c_state <= IDLE;
+	else      c_state <= SEND;
+
+end 
+
+////////Tx Input enable and Debounce avoidance//////////
+////////////////////////////////////////////////////////
+assign Dbnce = !d_shift_reg[1] & d_shift_reg[0];  /// Pernaei mono tin prwti timi pou tha dextei
+
+always @ (posedge Clock or posedge Reset) begin 
+
+	if(Reset) d_shift_reg <= 2'b00;
+
+	else d_shift_reg <= { d_shift_reg[0], Tx_en };
+
+end
+
+
+
+////  Write Enable statement ////
+/////////////////////////////////
+always @(c_state) begin
+
+	case(c_state)
+		
+		SEND: write_en <= 1'b1;
+
+		IDLE: write_en <= 1'b0;
+
+	endcase
+end
+
+/////////// Next step Decision /////////////
+////////////////////////////////////////////
+always @ (c_state or Dbnce or Message_in or Tx_done) begin 
+
+	case(c_state) 
+	
+		IDLE : begin
+	
+			if(Dbnce)  n_state <= SEND;
+			else       n_state <= IDLE;
+
+		end
+
+		SEND : begin 
+
+			if(Tx_done) n_state <= IDLE;
+			else        n_state <= SEND;
+
+		end
+
+		default             n_state <= IDLE;
+
+	endcase
+
+end 
+
+
+/////Procedure of sending data out via Tx pin///////
+////////////////////////////////////////////////////
 always @ (posedge Tick)  begin
 
 	if(!write_en) begin   // Leitoyrgei ws synchronized reset
@@ -50,10 +118,10 @@ always @ (posedge Tick)  begin
 		if( (counter == {4{1'b1}} ) && (!start_bit) && (bit_counter < N_bits - 1) ) begin 
 
 			msg_in_reg  <=  { 1'b0, msg_in_reg[7:1] };
-			bit_counter <= bit_counter +1;
-			t_out       <= msg_in_reg[0];
-			start_bit   <= 1'b0;
-			bit_counter <= 4'b0;
+			bit_counter <=  bit_counter +1;
+			t_out       <=  msg_in_reg[0];
+			start_bit   <=  1'b0;
+			bit_counter <=  4'b0;
 
 		end
 
@@ -75,16 +143,8 @@ always @ (posedge Tick)  begin
 
 		end 
 		
-
 	end
 
-
 end 
-
-
-
-
-
-
 
 endmodule 
